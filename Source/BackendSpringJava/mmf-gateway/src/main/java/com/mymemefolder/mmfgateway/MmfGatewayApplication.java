285fe1b784779769a4c0ae38fcbbbc6da4f72ced
Daniil Rodin <furidan@gmail.com>
2020-05-05 19:53:15 +0300

    Created the Spring Java based backend project

---
+++ Source/BackendSpringJava/mmf-gateway/src/main/java/com/mymemefolder/mmfgateway/MmfGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mymemefolder.mmfgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmfGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmfGatewayApplication.class, args);
+    }
+
+}
